b9b7e15d37aa43fabb3a9ee53161c6ae9d350d3a
shreyashc <shreyashc018@gmail.com>
2021-01-05 16:32:14 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incr = () => {
+    setCount((initalCount) => {
+      return initalCount + 1;
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="info">
+        <div className="progress">Work in Progeress</div>
+        <div className="emoji">🙂</div>
+      </div>
+      <div className="count">{count}</div>
+      <button onClick={incr}>Click me</button>
     </div>
   );
 }
